dbf72ba1b894848f698fa08b24ff14d05ca93acc
PolySim <simon.desdevises@gmail.com>
2022-07-31 18:06:05 +0200

    Change Image Battery

--- frontend/src/composent/Left/PhoneBack/TopBarre/index.tsx
+++ frontend/src/composent/Left/PhoneBack/TopBarre/index.tsx
@@ -13,7 +13,19 @@ export default function TopBarre(): JSX.Element {
       <Battery>
         <span>{battery}</span>%
       </Battery>
-      <img src={require("./1.png")} alt="batterie" height={"100%"} />
+      {parseInt(battery) === 0 ? (
+        <img src={require("./6.png")} alt="batterie" height={"100%"} />
+      ) : parseInt(battery) <= 20 ? (
+        <img src={require("./5.png")} alt="batterie" height={"100%"} />
+      ) : parseInt(battery) <= 40 ? (
+        <img src={require("./4.png")} alt="batterie" height={"100%"} />
+      ) : parseInt(battery) <= 60 ? (
+        <img src={require("./3.png")} alt="batterie" height={"100%"} />
+      ) : parseInt(battery) <= 80 ? (
+        <img src={require("./2.png")} alt="batterie" height={"100%"} />
+      ) : (
+        <img src={require("./1.png")} alt="batterie" height={"100%"} />
+      )}
     </Barre>
   );
 }
